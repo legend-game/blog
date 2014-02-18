625fe3f76a362b6d2e7cb708143d2325a46470c6
Duan Hongyi <duanhongyi@doopai.com>
2014-02-18 21:02:35 +0800

    add url redirect view

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
+from weblog.sitemaps import WeblogSitemap
+sitemaps = {
+    'weblog': WeblogSitemap,
+}
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'bblog.views.home', name='home'),
-    url(r'^blog/', include('weblog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='weblog/'), name='home'),
+    url(r'weblog/', include('weblog.urls')),
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap',
+            {'sitemaps':sitemaps}),
     url(r'^admin/', include(admin.site.urls)),
 )
 
